422153011dbdeb419a3bc0fc3573d7ee342d8cbb
Sergei Turchanov <turchanov@farpost.com>
2020-02-12 12:32:59 +1000

    Fix processing /proc/zoneinfo. Split node and zone stats.
    1. File /proc/zoneinfo contains information about two different entities:
    node stats and zone stats. Kernel developers decided not to create
    a dedicated file in /proc for node stats but mix-in those stats
    in zoneinfo (linux-kernel commit e2ecc8a79ed49f7838b4fdf352c4c48cec9424ac)
    under the first populated zone in each node.
    
    Current implementation /proc/zoneinfo parsing is incorrect as it tries
    to merge attributes of different entities into a single frankenstein
    structure Zoneinfo, which is a bad design pattern. Moreover, due to
    this decision, there is a bug in parsing data of the first zone in node.
    
    Given the /proc/zoneinfo lines:
    
    Node 0, zone      DMA
      per-node stats
          ...
      pages free x
          ...
    
    the resulting Zoneinfo element will have 'Zone' field empty (that is zone
    name "DMA" is discarded) as the current code interprets "per-node stats"
    line as a marker that all of the following lines in this section will be
    related to node stats only. Which is incorrect, as "DMA" zone stats
    ("pages free" and below) will follow next.
    
    2. Renamed/removed some stats:
       "nr_mlock_stack" - cannot find any reference to it in kernel git
           nor can I see it in current kernels' /proc/zoneinfo (and test
           fixtures)
       "nr_mlock" - added instead
    
       added some stats that appeared in current kernels.
    
    Signed-off-by: Sergei Turchanov <turchanov@farpost.com>

--- zoneinfo.go
+++ zoneinfo.go
@@ -25,172 +25,292 @@ import (
 	"github.com/prometheus/procfs/internal/util"
 )
 
+// Tag values describe a source field from /proc/zoneinfo
+
+type ZoneStats struct {
+	Node             string
+	Zone             string
+	Free             *int64   `json:"free"`
+	Min              *int64   `json:"min"`
+	Low              *int64   `json:"low"`
+	High             *int64   `json:"high"`
+	Scanned          *int64   `json:"scanned"`
+	Spanned          *int64   `json:"spanned"`
+	Present          *int64   `json:"present"`
+	Managed          *int64   `json:"managed"`
+	NrInactiveAnon   *int64   `json:"nr_zone_inactive_anon"`
+	NrActiveAnon     *int64   `json:"nr_zone_active_anon"`
+	NrInactiveFile   *int64   `json:"nr_zone_inactive_file"`
+	NrActiveFile     *int64   `json:"nr_zone_active_file"`
+	NrUnevictable    *int64   `json:"nr_zone_unevictable"`
+	NrWritePending   *int64   `json:"nr_zone_write_pending"`
+	NrMlock          *int64   `json:"nr_mlock"`
+	NrPageTablePages *int64   `json:"nr_page_table_pages"`
+	NrKernelStack    *int64   `json:"nr_kernel_stack"`
+	NrBounce         *int64   `json:"nr_bounce"`
+	NrZsPages        *int64   `json:"nr_zspages"`
+	NrFreeCma        *int64   `json:"nr_free_cma"`
+	NumaHit          *int64   `json:"numa_hit"`
+	NumaMiss         *int64   `json:"numa_miss"`
+	NumaForeign      *int64   `json:"numa_foreign"`
+	NumaInterleave   *int64   `json:"numa_interleave"`
+	NumaLocal        *int64   `json:"numa_local"`
+	NumaOther        *int64   `json:"numa_other"`
+	Protection       []*int64 `json:"protection"`
+}
+
+type NodeStats struct {
+	Node                       string
+	NrInactiveAnon             *int64 `json:"nr_inactive_anon"`
+	NrActiveAnon               *int64 `json:"nr_active_anon"`
+	NrInactiveFile             *int64 `json:"nr_inactive_file"`
+	NrActiveFile               *int64 `json:"nr_active_file"`
+	NrUnevictable              *int64 `json:"nr_unevictable"`
+	NrSlabReclaimable          *int64 `json:"nr_slab_reclaimable"`
+	NrSlabUnreclaimable        *int64 `json:"nr_slab_unreclaimable"`
+	NrIsolatedAnon             *int64 `json:"nr_isolated_anon"`
+	NrIsolatedFile             *int64 `json:"nr_isolated_file"`
+	NrWorkingsetRefault        *int64 `json:"workingset_refault"`
+	NrWorkingsetActivate       *int64 `json:"workingset_activate"`
+	NrWorkingsetNodereclaim    *int64 `json:"workingset_nodereclaim"`
+	NrAnonPages                *int64 `json:"nr_anon_pages"`
+	NrMapped                   *int64 `json:"nr_mapped"`
+	NrFilePages                *int64 `json:"nr_file_pages"`
+	NrDirty                    *int64 `json:"nr_dirty"`
+	NrWriteback                *int64 `json:"nr_writeback"`
+	NrWritebackTemp            *int64 `json:"nr_writeback_temp"`
+	NrShmem                    *int64 `json:"nr_shmem"`
+	NrShmemHugepages           *int64 `json:"nr_shmem_hugepages"`
+	NrShmemPmdMapped           *int64 `json:"nr_shmem_pmdmapped"`
+	NrAnonTransparentHugepages *int64 `json:"nr_anon_transparent_hugepages"`
+	NrUnstable                 *int64 `json:"nr_unstable"`
+	NrVmscanWrite              *int64 `json:"nr_vmscan_write"`
+	NrVmscanImmediateReclaim   *int64 `json:"nr_vmscan_immediate_reclaim"`
+	NrDirtied                  *int64 `json:"nr_dirtied"`
+	NrWritten                  *int64 `json:"nr_written"`
+}
+
 // Zoneinfo holds info parsed from /proc/zoneinfo.
 type Zoneinfo struct {
-	Node                       string
-	Zone                       string
-	NrFreePages                *int64
-	Min                        *int64
-	Low                        *int64
-	High                       *int64
-	Scanned                    *int64
-	Spanned                    *int64
-	Present                    *int64
-	Managed                    *int64
-	NrActiveAnon               *int64
-	NrInactiveAnon             *int64
-	NrIsolatedAnon             *int64
-	NrAnonPages                *int64
-	NrAnonTransparentHugepages *int64
-	NrActiveFile               *int64
-	NrInactiveFile             *int64
-	NrIsolatedFile             *int64
-	NrFilePages                *int64
-	NrSlabReclaimable          *int64
-	NrSlabUnreclaimable        *int64
-	NrMlockStack               *int64
-	NrKernelStack              *int64
-	NrMapped                   *int64
-	NrDirty                    *int64
-	NrWriteback                *int64
-	NrUnevictable              *int64
-	NrShmem                    *int64
-	NrDirtied                  *int64
-	NrWritten                  *int64
-	NumaHit                    *int64
-	NumaMiss                   *int64
-	NumaForeign                *int64
-	NumaInterleave             *int64
-	NumaLocal                  *int64
-	NumaOther                  *int64
-	Protection                 []*int64
+	Nodes []*NodeStats
+	Zones []*ZoneStats
 }
+
+const (
+	ProcessingNodeStats = 1
+	ProcessingZoneStats = 2
+)
 
 var nodeZoneRE = regexp.MustCompile(`(\d+), zone\s+(\w+)`)
 
 // Zoneinfo parses an zoneinfo-file (/proc/zoneinfo) and returns a slice of
 // structs containing the relevant info.  More information available here:
 // https://www.kernel.org/doc/Documentation/sysctl/vm.txt
-func (fs FS) Zoneinfo() ([]Zoneinfo, error) {
+func (fs FS) Zoneinfo() (Zoneinfo, error) {
 	data, err := ioutil.ReadFile(fs.proc.Path("zoneinfo"))
 	if err != nil {
-		return nil, fmt.Errorf("error reading zoneinfo %s: %s", fs.proc.Path("zoneinfo"), err)
+		return Zoneinfo{}, fmt.Errorf("error reading zoneinfo %s: %s", fs.proc.Path("zoneinfo"), err)
 	}
 	zoneinfo, err := parseZoneinfo(data)
 	if err != nil {
-		return nil, fmt.Errorf("error parsing zoneinfo %s: %s", fs.proc.Path("zoneinfo"), err)
+		return Zoneinfo{}, fmt.Errorf("error parsing zoneinfo %s: %s", fs.proc.Path("zoneinfo"), err)
 	}
 	return zoneinfo, nil
 }
 
-func parseZoneinfo(zoneinfoData []byte) ([]Zoneinfo, error) {
+func (stats *NodeStats) parse(line string) {
+	parts := strings.Fields(line)
+	if len(parts) < 2 {
+		return
+	}
+	vp := util.NewValueParser(parts[1])
 
-	zoneinfo := []Zoneinfo{}
+	switch parts[0] {
+	case "nr_inactive_anon":
+		stats.NrInactiveAnon = vp.PInt64()
+	case "nr_active_anon":
+		stats.NrActiveAnon = vp.PInt64()
+	case "nr_inactive_file":
+		stats.NrInactiveFile = vp.PInt64()
+	case "nr_active_file":
+		stats.NrActiveFile = vp.PInt64()
+	case "nr_unevictable":
+		stats.NrUnevictable = vp.PInt64()
+	case "nr_slab_reclaimable":
+		stats.NrSlabReclaimable = vp.PInt64()
+	case "nr_slab_unreclaimable":
+		stats.NrSlabUnreclaimable = vp.PInt64()
+	case "nr_isolated_anon":
+		stats.NrIsolatedAnon = vp.PInt64()
+	case "nr_isolated_file":
+		stats.NrIsolatedFile = vp.PInt64()
+	case "workingset_refault":
+		stats.NrWorkingsetRefault = vp.PInt64()
+	case "workingset_activate":
+		stats.NrWorkingsetActivate = vp.PInt64()
+	case "workingset_nodereclaim":
+		stats.NrWorkingsetNodereclaim = vp.PInt64()
+	case "nr_anon_pages":
+		stats.NrAnonPages = vp.PInt64()
+	case "nr_mapped":
+		stats.NrMapped = vp.PInt64()
+	case "nr_file_pages":
+		stats.NrFilePages = vp.PInt64()
+	case "nr_dirty":
+		stats.NrDirty = vp.PInt64()
+	case "nr_writeback":
+		stats.NrWriteback = vp.PInt64()
+	case "nr_writeback_temp":
+		stats.NrWritebackTemp = vp.PInt64()
+	case "nr_shmem":
+		stats.NrShmem = vp.PInt64()
+	case "nr_shmem_hugepages":
+		stats.NrShmemHugepages = vp.PInt64()
+	case "nr_shmem_pmdmapped":
+		stats.NrShmemPmdMapped = vp.PInt64()
+	case "nr_anon_transparent_hugepages":
+		stats.NrAnonTransparentHugepages = vp.PInt64()
+	case "nr_unstable":
+		stats.NrUnstable = vp.PInt64()
+	case "nr_vmscan_write":
+		stats.NrVmscanWrite = vp.PInt64()
+	case "nr_vmscan_immediate_reclaim":
+		stats.NrVmscanImmediateReclaim = vp.PInt64()
+	case "nr_dirtied":
+		stats.NrDirtied = vp.PInt64()
+	case "nr_written":
+		stats.NrWritten = vp.PInt64()
+	}
+}
+
+func (stats *ZoneStats) parse(line string) {
+	parts := strings.Fields(line)
+	if len(parts) < 2 {
+		return
+	}
+
+	if (parts[0] == "pages") && (parts[1] == "free") {
+		vp := util.NewValueParser(parts[2])
+		stats.Free = vp.PInt64()
+		return
+	}
+
+	vp := util.NewValueParser(parts[1])
+
+	switch parts[0] {
+	case "min":
+		stats.Min = vp.PInt64()
+	case "low":
+		stats.Low = vp.PInt64()
+	case "high":
+		stats.High = vp.PInt64()
+	case "scanned":
+		stats.Scanned = vp.PInt64()
+	case "spanned":
+		stats.Spanned = vp.PInt64()
+	case "present":
+		stats.Present = vp.PInt64()
+	case "managed":
+		stats.Managed = vp.PInt64()
+	case "nr_zone_inactive_anon":
+		stats.NrInactiveAnon = vp.PInt64()
+	case "nr_zone_active_anon":
+		stats.NrActiveAnon = vp.PInt64()
+	case "nr_zone_inactive_file":
+		stats.NrInactiveFile = vp.PInt64()
+	case "nr_zone_active_file":
+		stats.NrActiveFile = vp.PInt64()
+	case "nr_zone_unevictable":
+		stats.NrUnevictable = vp.PInt64()
+	case "nr_zone_write_pending":
+		stats.NrWritePending = vp.PInt64()
+	case "nr_mlock":
+		stats.NrMlock = vp.PInt64()
+	case "nr_page_table_pages":
+		stats.NrPageTablePages = vp.PInt64()
+	case "nr_kernel_stack":
+		stats.NrKernelStack = vp.PInt64()
+	case "nr_bounce":
+		stats.NrBounce = vp.PInt64()
+	case "nr_zspages":
+		stats.NrZsPages = vp.PInt64()
+	case "nr_free_cma":
+		stats.NrFreeCma = vp.PInt64()
+	case "numa_hit":
+		stats.NumaHit = vp.PInt64()
+	case "numa_miss":
+		stats.NumaMiss = vp.PInt64()
+	case "numa_foreign":
+		stats.NumaForeign = vp.PInt64()
+	case "numa_interleave":
+		stats.NumaInterleave = vp.PInt64()
+	case "numa_local":
+		stats.NumaLocal = vp.PInt64()
+	case "numa_other":
+		stats.NumaOther = vp.PInt64()
+	case "protection:":
+		protectionParts := strings.Split(line, ":")
+		protectionValues := strings.Replace(protectionParts[1], "(", "", 1)
+		protectionValues = strings.Replace(protectionValues, ")", "", 1)
+		protectionValues = strings.TrimSpace(protectionValues)
+		protectionStringMap := strings.Split(protectionValues, ", ")
+		val, err := util.ParsePInt64s(protectionStringMap)
+		if err == nil {
+			stats.Protection = val
+		}
+	}
+}
+
+func parseZoneinfo(zoneinfoData []byte) (Zoneinfo, error) {
+	zoneinfo := Zoneinfo{}
 
 	zoneinfoBlocks := bytes.Split(zoneinfoData, []byte("\nNode"))
 	for _, block := range zoneinfoBlocks {
-		var zoneinfoElement Zoneinfo
-		lines := strings.Split(string(block), "\n")
+		var currentNode, currentZone string
+		var nodestats *NodeStats
+		var zonestats *ZoneStats
+
+		data := strings.Split(string(block), "\n")
+
+		// This must not happen but still we have to check the size of "data" before slicing it
+		if len(data) < 2 {
+			continue
+		}
+		header, lines := data[0], data[1:]
+
+		// First line must be "(Node )?\d+, zone\s+\w+" since we split zoneinfoData by "\nNode"
+		nodeZone := nodeZoneRE.FindStringSubmatch(header)
+		if nodeZone == nil {
+			continue
+		}
+		currentNode = nodeZone[1]
+		currentZone = nodeZone[2]
+		zonestats = &ZoneStats{Node: currentNode, Zone: currentZone}
+
+		state := ProcessingZoneStats
 		for _, line := range lines {
+			line = strings.TrimSpace(line)
 
-			if nodeZone := nodeZoneRE.FindStringSubmatch(line); nodeZone != nil {
-				zoneinfoElement.Node = nodeZone[1]
-				zoneinfoElement.Zone = nodeZone[2]
+			if strings.HasPrefix(line, "per-node stats") {
+				state = ProcessingNodeStats
+				nodestats = &NodeStats{Node: currentNode}
 				continue
-			}
-			if strings.HasPrefix(strings.TrimSpace(line), "per-node stats") {
-				zoneinfoElement.Zone = ""
-				continue
-			}
-			parts := strings.Fields(strings.TrimSpace(line))
-			if len(parts) < 2 {
-				continue
-			}
-			vp := util.NewValueParser(parts[1])
-			switch parts[0] {
-			case "nr_free_pages":
-				zoneinfoElement.NrFreePages = vp.PInt64()
-			case "min":
-				zoneinfoElement.Min = vp.PInt64()
-			case "low":
-				zoneinfoElement.Low = vp.PInt64()
-			case "high":
-				zoneinfoElement.High = vp.PInt64()
-			case "scanned":
-				zoneinfoElement.Scanned = vp.PInt64()
-			case "spanned":
-				zoneinfoElement.Spanned = vp.PInt64()
-			case "present":
-				zoneinfoElement.Present = vp.PInt64()
-			case "managed":
-				zoneinfoElement.Managed = vp.PInt64()
-			case "nr_active_anon":
-				zoneinfoElement.NrActiveAnon = vp.PInt64()
-			case "nr_inactive_anon":
-				zoneinfoElement.NrInactiveAnon = vp.PInt64()
-			case "nr_isolated_anon":
-				zoneinfoElement.NrIsolatedAnon = vp.PInt64()
-			case "nr_anon_pages":
-				zoneinfoElement.NrAnonPages = vp.PInt64()
-			case "nr_anon_transparent_hugepages":
-				zoneinfoElement.NrAnonTransparentHugepages = vp.PInt64()
-			case "nr_active_file":
-				zoneinfoElement.NrActiveFile = vp.PInt64()
-			case "nr_inactive_file":
-				zoneinfoElement.NrInactiveFile = vp.PInt64()
-			case "nr_isolated_file":
-				zoneinfoElement.NrIsolatedFile = vp.PInt64()
-			case "nr_file_pages":
-				zoneinfoElement.NrFilePages = vp.PInt64()
-			case "nr_slab_reclaimable":
-				zoneinfoElement.NrSlabReclaimable = vp.PInt64()
-			case "nr_slab_unreclaimable":
-				zoneinfoElement.NrSlabUnreclaimable = vp.PInt64()
-			case "nr_mlock_stack":
-				zoneinfoElement.NrMlockStack = vp.PInt64()
-			case "nr_kernel_stack":
-				zoneinfoElement.NrKernelStack = vp.PInt64()
-			case "nr_mapped":
-				zoneinfoElement.NrMapped = vp.PInt64()
-			case "nr_dirty":
-				zoneinfoElement.NrDirty = vp.PInt64()
-			case "nr_writeback":
-				zoneinfoElement.NrWriteback = vp.PInt64()
-			case "nr_unevictable":
-				zoneinfoElement.NrUnevictable = vp.PInt64()
-			case "nr_shmem":
-				zoneinfoElement.NrShmem = vp.PInt64()
-			case "nr_dirtied":
-				zoneinfoElement.NrDirtied = vp.PInt64()
-			case "nr_written":
-				zoneinfoElement.NrWritten = vp.PInt64()
-			case "numa_hit":
-				zoneinfoElement.NumaHit = vp.PInt64()
-			case "numa_miss":
-				zoneinfoElement.NumaMiss = vp.PInt64()
-			case "numa_foreign":
-				zoneinfoElement.NumaForeign = vp.PInt64()
-			case "numa_interleave":
-				zoneinfoElement.NumaInterleave = vp.PInt64()
-			case "numa_local":
-				zoneinfoElement.NumaLocal = vp.PInt64()
-			case "numa_other":
-				zoneinfoElement.NumaOther = vp.PInt64()
-			case "protection:":
-				protectionParts := strings.Split(line, ":")
-				protectionValues := strings.Replace(protectionParts[1], "(", "", 1)
-				protectionValues = strings.Replace(protectionValues, ")", "", 1)
-				protectionValues = strings.TrimSpace(protectionValues)
-				protectionStringMap := strings.Split(protectionValues, ", ")
-				val, err := util.ParsePInt64s(protectionStringMap)
-				if err == nil {
-					zoneinfoElement.Protection = val
-				}
+			} else if strings.HasPrefix(line, "pages free") {
+				state = ProcessingZoneStats
 			}
 
+			switch state {
+			case ProcessingNodeStats:
+				nodestats.parse(line)
+			case ProcessingZoneStats:
+				zonestats.parse(line)
+			}
 		}
 
-		zoneinfo = append(zoneinfo, zoneinfoElement)
+		if nodestats != nil {
+			zoneinfo.Nodes = append(zoneinfo.Nodes, nodestats)
+		}
+		zoneinfo.Zones = append(zoneinfo.Zones, zonestats)
 	}
 	return zoneinfo, nil
 }
--- zoneinfo_test.go
+++ zoneinfo_test.go
@@ -23,22 +23,57 @@ import (
 
 func TestZoneinfo(t *testing.T) {
 	fs := getProcFixtures(t)
-	protection1 := []*int64{newPInt64(0), newPInt64(2877), newPInt64(7826), newPInt64(7826), newPInt64(7826)}
-	protection2 := []*int64{newPInt64(0), newPInt64(0), newPInt64(4949), newPInt64(4949), newPInt64(4949)}
-	refs := []Zoneinfo{
-		{Node: "0", Zone: "", NrFreePages: newPInt64(3952), Min: newPInt64(33), Low: newPInt64(41), High: newPInt64(49), Spanned: newPInt64(4095), Present: newPInt64(3975), Managed: newPInt64(3956), NrActiveAnon: newPInt64(547580), NrInactiveAnon: newPInt64(230981), NrIsolatedAnon: newPInt64(0), NrAnonPages: newPInt64(795576), NrAnonTransparentHugepages: newPInt64(0), NrActiveFile: newPInt64(346282), NrInactiveFile: newPInt64(316904), NrIsolatedFile: newPInt64(0), NrFilePages: newPInt64(761874), NrSlabReclaimable: newPInt64(131220), NrSlabUnreclaimable: newPInt64(47320), NrKernelStack: newPInt64(0), NrMapped: newPInt64(215483), NrDirty: newPInt64(908), NrWriteback: newPInt64(0), NrUnevictable: newPInt64(115467), NrShmem: newPInt64(224925), NrDirtied: newPInt64(8007423), NrWritten: newPInt64(7752121), NumaHit: newPInt64(1), NumaMiss: newPInt64(0), NumaForeign: newPInt64(0), NumaInterleave: newPInt64(0), NumaLocal: newPInt64(1), NumaOther: newPInt64(0), Protection: protection1},
-		{Node: "0", Zone: "DMA32", NrFreePages: newPInt64(204252), Min: newPInt64(19510), Low: newPInt64(21059), High: newPInt64(22608), Spanned: newPInt64(1044480), Present: newPInt64(759231), Managed: newPInt64(742806), NrKernelStack: newPInt64(2208), NumaHit: newPInt64(113952967), NumaMiss: newPInt64(0), NumaForeign: newPInt64(0), NumaInterleave: newPInt64(0), NumaLocal: newPInt64(113952967), NumaOther: newPInt64(0), Protection: protection2},
+	refs := Zoneinfo{
+		Nodes: []*NodeStats{
+			{Node: "0", NrInactiveAnon: newPInt64(230981), NrActiveAnon: newPInt64(547580), NrInactiveFile: newPInt64(316904),
+				NrActiveFile: newPInt64(346282), NrUnevictable: newPInt64(115467), NrSlabReclaimable: newPInt64(131220),
+				NrSlabUnreclaimable: newPInt64(47320), NrIsolatedAnon: newPInt64(0), NrIsolatedFile: newPInt64(0),
+				/*TODO: what about workingset_nodes, workingset_restore? */
+				NrWorkingsetRefault: newPInt64(466886), NrWorkingsetActivate: newPInt64(276925), NrWorkingsetNodereclaim: newPInt64(487),
+				NrAnonPages: newPInt64(795576), NrMapped: newPInt64(215483), NrFilePages: newPInt64(761874), NrDirty: newPInt64(908),
+				NrWriteback: newPInt64(0), NrWritebackTemp: newPInt64(0), NrShmem: newPInt64(224925), NrShmemHugepages: newPInt64(0),
+				NrShmemPmdMapped: newPInt64(0), NrAnonTransparentHugepages: newPInt64(0), NrUnstable: newPInt64(0),
+				NrVmscanWrite: newPInt64(12950), NrVmscanImmediateReclaim: newPInt64(3033), NrDirtied: newPInt64(8007423),
+				NrWritten: newPInt64(7752121), /* nr_kernel_misc_reclaimable is not present in current kernels */
+			},
+		},
+		Zones: []*ZoneStats{
+			{Node: "0", Zone: "DMA", Free: newPInt64(3952), Min: newPInt64(33), Low: newPInt64(41), High: newPInt64(49),
+				Spanned: newPInt64(4095), Present: newPInt64(3975), Managed: newPInt64(3956), NrInactiveAnon: newPInt64(0),
+				NrActiveAnon: newPInt64(0), NrInactiveFile: newPInt64(0), NrActiveFile: newPInt64(0), NrUnevictable: newPInt64(0),
+				NrWritePending: newPInt64(0), NrMlock: newPInt64(0), NrPageTablePages: newPInt64(0), NrKernelStack: newPInt64(0),
+				NrBounce: newPInt64(0), NrZsPages: newPInt64(0), NrFreeCma: newPInt64(0), NumaHit: newPInt64(1), NumaMiss: newPInt64(0),
+				NumaForeign: newPInt64(0), NumaInterleave: newPInt64(0), NumaLocal: newPInt64(1), NumaOther: newPInt64(0),
+				Protection: []*int64{newPInt64(0), newPInt64(2877), newPInt64(7826), newPInt64(7826), newPInt64(7826)}},
+			{Node: "0", Zone: "DMA32", Free: newPInt64(204252), Min: newPInt64(19510), Low: newPInt64(21059), High: newPInt64(22608),
+				Spanned: newPInt64(1044480), Present: newPInt64(759231), Managed: newPInt64(742806), NrInactiveAnon: newPInt64(118558),
+				NrActiveAnon: newPInt64(106598), NrInactiveFile: newPInt64(75475), NrActiveFile: newPInt64(70293), NrUnevictable: newPInt64(66195),
+				NrWritePending: newPInt64(64), NrMlock: newPInt64(4), NrPageTablePages: newPInt64(1756), NrKernelStack: newPInt64(2208),
+				NrBounce: newPInt64(0), NrZsPages: newPInt64(0), NrFreeCma: newPInt64(0), NumaHit: newPInt64(113952967), NumaMiss: newPInt64(0),
+				NumaForeign: newPInt64(0), NumaInterleave: newPInt64(0), NumaLocal: newPInt64(113952967), NumaOther: newPInt64(0),
+				Protection: []*int64{newPInt64(0), newPInt64(0), newPInt64(4949), newPInt64(4949), newPInt64(4949)}},
+			{Node: "0", Zone: "Normal", Free: newPInt64(18553), Min: newPInt64(11176), Low: newPInt64(13842), High: newPInt64(16508),
+				Spanned: newPInt64(1308160), Present: newPInt64(1308160), Managed: newPInt64(1268711), NrInactiveAnon: newPInt64(112423),
+				NrActiveAnon: newPInt64(440982), NrInactiveFile: newPInt64(241429), NrActiveFile: newPInt64(275989), NrUnevictable: newPInt64(49272),
+				NrWritePending: newPInt64(844), NrMlock: newPInt64(154), NrPageTablePages: newPInt64(9750), NrKernelStack: newPInt64(15136),
+				NrBounce: newPInt64(0), NrZsPages: newPInt64(0), NrFreeCma: newPInt64(0), NumaHit: newPInt64(162718019), NumaMiss: newPInt64(0),
+				NumaForeign: newPInt64(0), NumaInterleave: newPInt64(26812), NumaLocal: newPInt64(162718019), NumaOther: newPInt64(0),
+				Protection: []*int64{newPInt64(0), newPInt64(0), newPInt64(0), newPInt64(0), newPInt64(0)}},
+			{Node: "0", Zone: "Movable", Free: newPInt64(0), Min: newPInt64(0), Low: newPInt64(0), High: newPInt64(0),
+				Spanned: newPInt64(0), Present: newPInt64(0), Managed: newPInt64(0),
+				Protection: []*int64{newPInt64(0), newPInt64(0), newPInt64(0), newPInt64(0), newPInt64(0)}},
+			{Node: "0", Zone: "Device", Free: newPInt64(0), Min: newPInt64(0), Low: newPInt64(0), High: newPInt64(0),
+				Spanned: newPInt64(0), Present: newPInt64(0), Managed: newPInt64(0),
+				Protection: []*int64{newPInt64(0), newPInt64(0), newPInt64(0), newPInt64(0), newPInt64(0)}},
+		},
 	}
 	data, err := fs.Zoneinfo()
 	if err != nil {
 		t.Fatalf("failed to parse zoneinfo: %v", err)
 	}
 
-	for index, ref := range refs {
-		want, got := ref, data[index]
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Fatalf("unexpected crypto entry (-want +got):\n%s", diff)
-		}
-
+	want, got := refs, data
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("unexpected zoneinfo entry (-want +got):\n%s", diff)
 	}
 }
